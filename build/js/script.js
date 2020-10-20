d92b56862ec0a58747668f25270f9b5c1fe4f215
Elena Ordina <post032@yandex.ru>
2020-10-20 18:43:27 +0300

    adaptive

--- build/js/script.js
+++ build/js/script.js
@@ -15,3 +15,40 @@ menuInner.addEventListener('mouseleave', function(e) {
   menuInner.classList.remove('active');
   menuCatalog.classList.remove('active');
 })
+
+if (innerWidth < 769) {
+  let minMenuElements = document.querySelectorAll('.menu-min .menu-inner__title');
+  let footerMenuTitles = document.querySelectorAll('.footer .fourColumns li .title');
+  let burgerMenu = document.querySelector('.top .menu .burger');
+
+  for (let footerMenuTitle of footerMenuTitles) {
+    footerMenuTitle.addEventListener('click', function(e) {
+      e.preventDefault();
+      let parentMenu = footerMenuTitle.parentNode;
+      parentMenu.classList.toggle('show');
+    });
+  }
+  for (let minMenuElement of minMenuElements) {
+    minMenuElement.addEventListener('click', function(e) {
+      if(minMenuElement.nextElementSibling) {
+        e.preventDefault();
+        let parentMenuItem = minMenuElement.parentNode;
+        parentMenuItem.classList.toggle('active');
+      }
+    });
+  }
+
+  burgerMenu.addEventListener('click', function(e) {
+    e.preventDefault();
+    burgerMenu.nextElementSibling.classList.toggle('active');
+  })
+}
+if (innerWidth < 961) {
+  let mobilePhone = document.querySelector('.mobile-phone');
+  console.log(mobilePhone);
+  mobilePhone.addEventListener('click', function (e) {
+    e.preventDefault();
+    mobilePhone.nextElementSibling.classList.toggle('active');
+    console.log(mobilePhone.nextElementSibling);
+  });
+}
